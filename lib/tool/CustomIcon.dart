f3c8303d52d6cfd0bea474572711d427f6c4e1bd
DannyCX <2258186062@qq.com>
2023-11-08 07:31:43 +0800

    自定义icon

---
+++ lib/tool/CustomIcon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon {
+  static const IconData tool_pencil = IconData(
+      0xe602,
+      fontFamily: 'CustomIcon',
+      matchTextDirection: true
+  );
+}
