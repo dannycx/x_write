469c0da94d7eacf6c4595da63fe69f1ba156c06c
DannyCX <33610732+dannycx@users.noreply.github.com>
2023-11-30 11:35:39 +0800

    Update paint_state.dart

--- lib/model/render/data/paint_state.dart
+++ lib/model/render/data/paint_state.dart
@@ -9,7 +9,14 @@ enum ShapeType {
 
 /// 操作类型：
 enum OpType {
-  pen, pencil, highlight, brush, shape, image, text, doc, video
+  // 工具：钢笔，铅笔，荧光笔，毛笔，图形，AI，套索，板擦，反撤销，撤销
+  pen, pencil, highlight, brush, shape, ai, lasso, eraser, undo, redo,
+
+  // 多媒体：图片，文本，文档，视频
+  image, text, doc, video,
+
+  // options：尺子，截屏，搜图，录屏，便签，计时器，放大镜，扫一扫
+  ruler, screenshot, search_image, recording, sticky_note, timer, magnifier, scan
 }
 
 /// 通用事件类型
